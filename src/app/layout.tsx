975cf8249843752c3ed5a7b3f82114a7ce6cca5e
Nwali Somto <somtobernardnwali619@gmail.com>
2025-02-23 15:09:26 +0100

    second commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,19 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+// import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import { Toaster } from "@/components/ui/sonner";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+// const geistSans = Geist({
+//   variable: "--font-geist-sans",
+//   subsets: ["latin"],
+// });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+// const geistMono = Geist_Mono({
+//   variable: "--font-geist-mono",
+//   subsets: ["latin"],
+// });
 
 export const metadata: Metadata = {
   title: "SBN Solar Company",
@@ -29,7 +29,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased flex flex-col justify-between min-h-screen w-full font-sans overflow-x-hidden`}
+        className={` antialiased flex flex-col justify-between min-h-screen w-full font-sans overflow-x-hidden`}
       >
         <Navbar />
         <main className="flex-grow">{children}</main>
